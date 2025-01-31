1cc22631be3a226e02b2294c40cd059bab62bdc3
Henrique Tavares <dev@newlivseguros.com>
2025-01-31 02:14:43 -0300

    Ajusta configurações de DEBUG e CSRF para debug do login

--- newliv_crm/settings.py
+++ newliv_crm/settings.py
@@ -29,10 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+ALLOWED_HOSTS = ['*'] if DEBUG else os.getenv('ALLOWED_HOSTS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+    'https://*.newlivseguros.com',
+] if not DEBUG else []
 
 
 # Application definition
