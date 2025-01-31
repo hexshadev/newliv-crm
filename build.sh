8a2ddc0bdc195c3cd535ac7d3554c85036612229
Henrique Tavares <dev@newlivseguros.com>
2025-01-31 02:01:53 -0300

    Adiciona script de build para o Render.com

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# Instala as dependências do Python
 pip install -r requirements.txt
 
+# Coleta arquivos estáticos
 python manage.py collectstatic --no-input
+
+# Executa as migrações
 python manage.py migrate 
